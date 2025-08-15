079aeeaf8b4e2ed4fac56ad0cde6db5602ec6194
JumaboyevJasurbek <jumaboyevjasurbek2004@gmail.com>
2025-08-15 10:18:10 +0500

    fix api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Question, QuestionWithAnswer, TestResult } from "../types";
 
-const API_BASE_URL = process.env.API_BASE_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
